fcffe1b784a13b06dad821eeb547a4f9dfced588
kda2019 <kda2019@gmail.com>
2021-03-23 16:42:53 +0200

    task 2 v2

---
+++ hello.py
@@ -0,0 +1,6 @@
+def wsgi_app(env, start_response):
+
+	body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	return body
--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo gunicorn --bind='0.0.0.0:8080' -c /home/box/web/etc/hello.py hello:wsgi_app
+sudo gunicorn --bind='0.0.0.0:8080' hello:wsgi_app
 sudo /etc/init.d/nginx restart
