c4f1fdd8acbc005bbbbe669908e8ae5953274bfa
kda2019 <kda2019@gmail.com>
2021-03-23 18:35:47 +0200

    task2 v3

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def wsgi_app(env, start_response):
 
-	body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+
+	body = [f'{i}\n'.encode() for i in env['QUERY_STRING'].split('&')]
 
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return body
